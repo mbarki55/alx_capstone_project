05a2449cca07c739b3bbf12ac8b3aba9f9586832
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-03-16 16:17:55 +0100

    sign up

--- capstone_web_app/capstone_backend/signup.py
+++ capstone_web_app/capstone_backend/signup.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+import re
 
 app = Flask(__name__)
 app.secret_key='I will do that later'
@@ -16,16 +17,34 @@ mysql =MySQL(app)
 def index():
     return render_template('signup.html')
 
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    new_username = request.form['new_username']
-    new_password = request.form['new_password']
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (new_username,new_password))
-    mysql.connection.commit()
-    cur.close()
-    # Add your sign up logic here
-    return f'Sign up attempt with username: {new_username} and password: {new_password}'
+    message=''
+    if  request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
+        new_username = request.form['new_username']
+        new_email = request.form['new_email']
+        new_password = request.form['new_password']
+        confirm_new_password = request.form['confirm_new_password']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (username, password, email) VALUES (%s, %s)", (new_username, new_password, confirm_new_password, new_email))
+        mysql.connection.commit()
+        account = cur.fetchone()
+        if account:
+            message = 'Account already exists'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', new_email): 
+            message = 'Invalid email address !'
+        elif new_password != confirm_new_password:
+            message = 'Passwords do not match!'
+        elif not new_username or not new_password or not confirm_new_password or not new_email:
+            message= 'Please fill out the form '
+        else:
+            cur.execute('INSERT INTO user VALUES(NULL,% s, % s, % s, % s)',(new_username, new_email, new_password, confirm_new_password))
+            mysql.connection.commit()
+            message = 'You have successfully registered!' 
+    elif request.method == 'POST':
+        message = 'Please fill out the form !'
+    return render_template('register.html', message= message)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
