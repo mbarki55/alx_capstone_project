c1eb1fe88e443045c160243b1f2852bd241dc27f
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-03-17 01:41:12 +0100

    this is last sign up

--- capstone_web_app/capstone_frontend/app.py
+++ capstone_web_app/capstone_frontend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_mysqldb import MySQL
 import MySQLdb
 
 app = Flask(__name__)
+app.secret_key='kjhbd67hjsk'
+
 app.config['MYSQL_HOST'] = 'localhost'  
 app.config['MYSQL_USER'] = 'root'      
 app.config['MYSQL_PASSWORD'] = '<@Oumaima55>'       
@@ -16,22 +18,26 @@ def index():
             username = request.form["username"]
             email = request.form["email"]
             password = request.form["password"]
+            new_password = request.form["new_password"]
 
             conn = MySQLdb.connect(host='localhost', user='root', password='<@Oumaima55>', database='capstone_project')
 
             if conn:
                 cur = conn.cursor()
-                cur.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password))
+                cur.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password, new_password))
                 conn.commit()
                 cur.close()
                 conn.close() 
-                return render_template("success.html")
+                flash("successful registration")
             else:
-                return render_template("error.html", error="Failed to establish database connection")
+                flash("Failed to establish database connection")
+            if password != new_password:
+                flash("Passwords do not match!")
+            
         except Exception as e:
             return render_template("error.html", error=str(e))
 
-    return render_template('index.html')
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
